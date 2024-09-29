d22e919cea4cb3cdadbaa3dfff782f08f01a13b1
Jillian Tanda <jilliantanda@gmail.com>
2024-09-28 20:04:29 -0400

    about finished

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -6,13 +6,22 @@ const About = (props) => {
          <>
          {<DashNav/>}
 <div className="about">
-        <h1>DisArray </h1>
-        <div>
+          <div className="about1">
+<img
+          src="https://i.imgur.com/WTTUhfL.png"
+          width="30%"
+          height="30%"
+        ></img>
+  
         <p>Welcome to DiAarray, your all-in-one productivity tool designed to help you set and achieve your goals while staying on top of industry best practices and emerging technologies. </p>
-        <p>
-        Whether you're a professional aiming to sharpen your skills, a student navigating the learning curve, or a team looking to streamline workflows, DisArray provides the organization and insight you need to thrive.</p></div>
+       </div>
 
-   <div>     
+       <div className="about5">
+ <p>
+        Whether you're a professional aiming to sharpen your skills, a student navigating the learning curve, or a team looking to streamline workflows, DisArray provides the organization and insight you need to thrive.</p>
+        </div>
+        
+        <div className="about2">
 <p>
 
    
@@ -26,7 +35,7 @@ const About = (props) => {
 <b>Stay Current: </b>The tech industry evolves rapidly. Disarray ensures you’re always in the loop with curated updates on industry trends, new technologies, and best practices tailored to your interests.
 </p>
  </div>
- <div>
+ <div className="about3">
 <p>
 <b>Why DisArray?</b><br/> <br />
 We know how challenging it can be to balance learning, growth, and staying organized. DisArray simplifies this process, empowering you to manage your professional or personal development efficiently. Whether you're an individual or a team, DisArray is here to ensure you don’t just keep up with the pace of innovation—you thrive in it.
