3f654fb1fe4dcd7bdce26f0dd69ce3515581e48c
Jillian Tanda <jilliantanda@gmail.com>
2024-09-30 16:04:23 -0400

    sign up and log in forms created

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -5,35 +5,71 @@ import Form from "react-bootstrap/Form";
 
 const LoginSignup = () => {
   return (
-    <div className="signup">
-        <div>
-        <img src="https://i.imgur.com/MfHAsXS.png"></img>
-        </div>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control size="lg" type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-4" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control size="lg" type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-4" controlId="formBasicPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control size="lg" type="password" placeholder="Confirm Password" />
-        </Form.Group>
-        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <>
+      <div className="logcontainer">
+        <div className="signup">
+          <div>
+            <img src="https://i.imgur.com/MfHAsXS.png"></img>
+          </div>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control size="lg" type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Username</Form.Label>
+              <Form.Control size="lg" type="email" placeholder="Enter username" />
+              <Form.Text className="text-muted">
+                This name will be displayed on your dashboard
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control size="lg" type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                size="lg"
+                type="password"
+                placeholder="Confirm Password"
+              />
+            </Form.Group>
+            {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group> */}
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
+            <Button variant="primary" type="submit">
+              Create Account
+            </Button>
+          </Form>
+        </div>
+        <div className="signin">
+          <div>
+            <img src="https://i.imgur.com/MfHAsXS.png"></img>
+          </div>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control size="lg" type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control size="lg" type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-1" controlId="formBasicPassword">
+              <Form.Label>Forgot Password?</Form.Label>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Forgot Email?</Form.Label>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Log In
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </>
   );
 };
 
