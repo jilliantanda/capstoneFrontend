7ba7bf1b569acb6ab64ffd3c64d5a2bea6535b9e
Jillian Tanda <jilliantanda@gmail.com>
2024-10-02 18:51:26 -0400

    removed from app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
 import Home from "./components/Home";
-import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-       <BrowserRouter>
       <Home />
-    </BrowserRouter>
     </div>
   );
 }
